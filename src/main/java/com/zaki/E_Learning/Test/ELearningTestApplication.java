3b7155d7fa15724a42f2a93f27b7c40cd4d2b75e
Zaki dono <zakariayousfi@gmail.com>
2024-01-29 08:38:43 +0100

    Spring Test

---
+++ src/main/java/com/zaki/E_Learning/Test/ELearningTestApplication.java
@@ -0,0 +1,13 @@
+package com.zaki.E_Learning.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaki/E_Learning/Test/ELearningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaki.E_Learning.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
